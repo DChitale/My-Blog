b7de098c3e235ac3251b981385e82593377d683b
DChitale <chitaledhananjay70@gmail.com>
2026-05-22 09:43:02 +0530

    Added health route

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,11 @@ const connectDB = require('./src/db/db');
 
 connectDB();
 
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Server is healthy' });
+});
+
 app.listen(3000,()=>{
     console.log("Connected to Backend");
 })
